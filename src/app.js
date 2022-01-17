e75fdacdf5fb52036288c48eeccbc1eb39180e66
DevOpsLuixus <ing.luismadrid@gmail.com>
2022-01-17 17:34:45 +0000

    sync

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numberArray = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let arrayIcon = ["\u2665", "\u2666", "\u2663", "\u2660"];
+
+  function randoms(array) {
+    let randomCard = Math.floor(Math.random() * array.length);
+    return randomCard;
+  }
+  let numbersArray = numberArray[randoms(numberArray)];
+  let arrayIcons = arrayIcon[randoms(arrayIcon)];
+
+  if (arrayIcons == "\u2665" || arrayIcons == "\u2666") {
+    document.querySelector("#leftIcon").style.color = "red";
+    document.querySelector("#righIcon").style.color = "red";
+    document.querySelector("#num").style.color = "red";
+  } else {
+    document.querySelector("#leftIcon").style.color = "black";
+    document.querySelector("#righIcon").style.color = "black";
+    document.querySelector("#num").style.color = "black";
+  }
+
+  document.getElementById("num").innerHTML = numbersArray;
+  document.getElementById("leftIcon").innerHTML = arrayIcons;
+  document.getElementById("righIcon").innerHTML = arrayIcons;
 };
